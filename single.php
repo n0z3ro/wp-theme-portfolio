2630f036eef660e37a984fa4e76dba3a461ad13a
Captain n0z3ro <admin@n0z3ro.com>
2014-12-29 00:06:53 -0600

    touch of style update

--- single.php
+++ single.php
@@ -1,14 +1,18 @@
 <?php get_header(); ?>
 
 <div id="primary" class="content-area">
-  <?php while ( have_posts() ) : the_post();
+  <?php
+    while ( have_posts() ) : the_post();
     the_title('<h2 class="sub-header">', '</h2>'); 
     if (has_post_thumbnail()){
       echo '<div class="primary-img">';
       the_post_thumbnail();
       echo '</div>';
     }
-    the_content(); ?>
+  ?>
+  <div class="post-content">
+    <?php the_content(); ?>
+  </div>
 
   <?php endwhile; ?>
 
