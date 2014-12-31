545148e5bd528d4e3f8d7a21991d4777041583f5
Captain n0z3ro <admin@n0z3ro.com>
2014-12-31 04:43:19 -0600

    add thumbnail to prev and next links

--- single.php
+++ single.php
@@ -12,33 +12,36 @@
       if (has_post_thumbnail()){
   ?>
         <div class="primary-img">
-          <div class="prev-post">
-            <?php if (get_adjacent_post( true, '', true)){ ?>
-              <?php previous_post_link('<div>%link</div>', 'Previous post in category', TRUE); ?>
-            <?php }else{ ?>
-              no prev post
-            <?php }; ?>
-          </div>
-          <?php the_post_thumbnail(); ?>
-          <div class="next-post">
-            <?php if (get_adjacent_post( true, '', false)){ ?>
-              <?php next_post_link('%link', 'Next post in category', TRUE); ?>
-            <?php }else{ ?>
+          <div class="next-post-wrap post-nav-wrap">
+            <?php
+              if (get_adjacent_post( true, '', false)){
+                $nextPost = get_next_post(true);
+                $nextthumbnail = get_the_post_thumbnail($nextPost->ID, array(100,100) );
+                next_post_link('<div class="prev-post-link nav-post-link">%link</div>', $nextthumbnail, TRUE);
+              }else{
+            ?>
               no next post
             <?php }; ?>
           </div>
+          <?php the_post_thumbnail(); ?>
+          <div class="post-content">
+            <?php the_content(); ?>
+          </div>
+          <div class="prev-post-wrap post-nav-wrap">
+            <?php
+              if (get_adjacent_post( true, '', true)){
+                $prevPost = get_previous_post(true);
+                $prevthumbnail = get_the_post_thumbnail($prevPost->ID, array(100,100) );
+                previous_post_link('<div class="prev-post-link nav-post-link">%link</div>', $prevthumbnail, TRUE);
+              }else{
+            ?>
+              no prev post
+            <?php }; ?>
+          </div>
         </div>
-  <?php
-      }
-  ?>
-  
-  <div class="post-content">
-    <?php the_content(); ?>
-  </div>
-  
+  <?php } ?>
   <?php endwhile; ?>
   <?php wp_reset_query() ?>
 </div><!-- #primary .content-area -->
-
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
