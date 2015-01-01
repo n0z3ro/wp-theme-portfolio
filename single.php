fd203af40036f330cc96ae1282a62196011dc723
Captain n0z3ro <admin@n0z3ro.com>
2014-12-31 20:48:37 -0600

    style prev and next post links

--- single.php
+++ single.php
@@ -11,33 +11,37 @@
       
       if (has_post_thumbnail()){
   ?>
-        <div class="primary-img">
+        <div class="post-wrap">
           <div class="next-post-wrap post-nav-wrap">
             <?php
               if (get_adjacent_post( true, '', false)){
                 $nextPost = get_next_post(true);
-                $nextthumbnail = get_the_post_thumbnail($nextPost->ID, array(100,100) );
-                next_post_link('<div class="prev-post-link nav-post-link">%link</div>', $nextthumbnail, TRUE);
+                $nextthumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($nextPost->ID), array(100,100), false, '');
+                (string)$nextthumbnail = $nextthumbnail[0];
+                next_post_link('<div class="next-post-link nav-post-link" style="background:url('.get_stylesheet_directory_uri().'/images/dark-nav-arrow-left.png) 0 0 / 100px 100px no-repeat, url('.$nextthumbnail.') 0 0 / 100px 100px no-repeat;">%link</div>', '%title', TRUE);
               }else{
             ?>
               no next post
             <?php }; ?>
           </div>
-          <?php the_post_thumbnail(); ?>
-          <div class="post-content">
-            <?php the_content(); ?>
+          <div class="primary-img">
+            <?php the_post_thumbnail(); ?>
           </div>
           <div class="prev-post-wrap post-nav-wrap">
             <?php
               if (get_adjacent_post( true, '', true)){
                 $prevPost = get_previous_post(true);
-                $prevthumbnail = get_the_post_thumbnail($prevPost->ID, array(100,100) );
-                previous_post_link('<div class="prev-post-link nav-post-link">%link</div>', $prevthumbnail, TRUE);
+                $prevthumbnail = wp_get_attachment_image_src(get_post_thumbnail_id($prevPost->ID), array( 100,100 ), false, '');
+                (string)$prevthumbnail = $prevthumbnail[0];
+                previous_post_link('<div class="prev-post-link nav-post-link" style="background:url('.get_stylesheet_directory_uri().'/images/dark-nav-arrow-right.png) 0 0 / 100px 100px no-repeat, url('.$prevthumbnail.') 0 0 / 100px 100px no-repeat;">%link</div>', '%title', TRUE);
               }else{
             ?>
               no prev post
             <?php }; ?>
           </div>
+        </div>
+        <div class="post-content">
+          <?php the_content(); ?>
         </div>
   <?php } ?>
   <?php endwhile; ?>
