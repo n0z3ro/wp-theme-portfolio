1b4ac76f715dd93a42298e3e6f3968aa0ba352b4
Captain n0z3ro <admin@n0z3ro.com>
2015-06-11 22:40:19 -0500

    add footer style and info

--- footer.php
+++ footer.php
@@ -2,6 +2,12 @@
     </div><!-- #main .site-main -->
       <footer id="colophon" class="site-footer" role="contentinfo">
         <div class="site-info">
+        	<div class="foot-copyright">&#169; <?php echo date("Y"); ?></div>
+			<div class="foot-soc-links">
+				<a class="youtube-link" href="https://www.youtube.com/user/n0z3ro">YouTube</a>
+				<a class="linkedin-link" href="https://www.linkedin.com/in/n0z3ro">LinkedIn</a>
+				<a class="twitter-link" href="https://twitter.com/n0z3ro">Twitter</a>
+			</div>
         </div><!-- .site-info -->
       </footer><!-- #colophon .site-footer -->
     </div><!-- #page .hfeed .site -->
