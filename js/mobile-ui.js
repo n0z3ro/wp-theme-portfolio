ea4e47eec017a003783a5a9de8bb580e74015be2
Captain n0z3ro <admin@n0z3ro.com>
2015-06-29 23:29:29 -0500

    basic mobile nav implemented

--- js/mobile-ui.js
+++ js/mobile-ui.js
@@ -1,3 +1,17 @@
 window.onload = function () {
- //alert('Hello');
+
+  var mobilemenu = document.getElementById("menu-main-nav");
+
+  var mobiletag = document.getElementsByClassName("mobile-tag");
+
+  mobiletag[0].addEventListener("click", whatClicked, false);
+  
+  function whatClicked(evt) {
+
+    mobilemenu.style.height = "auto";
+
+    //mobiletag.addclass(toggleOpen) or (toggleClosed)
+
+  };
+
 };
