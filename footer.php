62e28ba519ddbd6d9aad20dd41c702f60e824bb0
Captain n0z3ro <admin@n0z3ro.com>
2015-07-03 06:13:12 -0500

    update footer social links to reflect current strongest line up

--- footer.php
+++ footer.php
@@ -4,9 +4,10 @@
         <div class="site-info">
         	<div class="foot-copyright">&#169; <?php echo date("Y"); ?></div>
 			<div class="foot-soc-links">
-				<a class="youtube-link" href="https://www.youtube.com/user/n0z3ro">YouTube</a>
-				<a class="linkedin-link" href="https://www.linkedin.com/in/n0z3ro">LinkedIn</a>
-				<a class="twitter-link" href="https://twitter.com/n0z3ro">Twitter</a>
+        <a class="googleplus-link" href="https://plus.google.com/112599957781660056127/posts" target="_blank">Google+</a>
+        <a class="twitchtv-link" href="http://www.twitch.tv/n0z3ro" target="_blank">TwitchTV</a>
+        <a class="youtube-link" href="https://www.youtube.com/user/n0z3ro" target="_blank">YouTube</a>
+        <a class="linkedin-link" href="https://www.linkedin.com/in/n0z3ro" target="_blank">LinkedIn</a>
 			</div>
         </div><!-- .site-info -->
       </footer><!-- #colophon .site-footer -->
