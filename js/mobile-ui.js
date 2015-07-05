e582ea4073385eb72e9a212633663a7affaaaab5
Captain n0z3ro <admin@n0z3ro.com>
2015-07-05 03:25:13 -0500

    mobile menu javascript toggle

--- js/mobile-ui.js
+++ js/mobile-ui.js
@@ -1,16 +1,26 @@
 window.onload = function () {
 
   var mobilemenu = document.getElementById("menu-main-nav");
-
   var mobiletag = document.getElementsByClassName("mobile-tag");
 
-  mobiletag[0].addEventListener("click", whatClicked, false);
+  mobiletag[0].addEventListener("click", toggleMobile, false);
   
-  function whatClicked(evt) {
+  function toggleMobile(evt) {
+    
+    var mobileClasses = mobilemenu.classList;
 
-    mobilemenu.style.height = "auto";
-
-    //mobiletag.addclass(toggleOpen) or (toggleClosed)
+    if (mobileClasses.contains("mobile-open")){
+      mobileClasses.remove("mobile-open");
+      mobileClasses.add("mobile-closed");
+      mobilemenu.style.height = "30px";
+    } else if (mobileClasses.contains("mobile-closed")){
+      mobileClasses.remove("mobile-closed");
+      mobileClasses.add("mobile-open");
+      mobilemenu.style.height = "auto";
+    } else { //has neither class - uninitiated menu
+      mobileClasses.add("mobile-open");
+      mobilemenu.style.height = "auto";
+    };
 
   };
 
